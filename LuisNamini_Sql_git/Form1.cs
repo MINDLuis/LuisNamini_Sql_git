9ffac864d147ffb805b18748daeeec2e9bd94a50
naminilu <naminilu@E211-23-008>
2026-03-16 10:44:00 +0100

    test kommentar ergänzt

--- LuisNamini_Sql_git/Form1.cs
+++ LuisNamini_Sql_git/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//hallo
 namespace LuisNamini_Sql_git
 {
     public partial class Form1 : System.Windows.Forms.Form
